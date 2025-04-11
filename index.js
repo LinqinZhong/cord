84a6fe360ff258c004075ec13d78f0d4e32b3f80
linqin.zhong@rastargame.com <2952868313@qq.com>
2025-04-11 17:33:00 +0800

    feat: 添加发音

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const handleLearn = require("./lib/learn");
 const rl = require("./lib/readline");
 
-handleLearn().then(() => {
+handleLearn().then(() => { }).catch((err) => {
+  console.log('err', err);
+}).finally(() => {
   rl.close()
 })
